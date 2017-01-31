ba07c49244a42f81d8b80a536b80ce80aa24de08
Davit Dovlatian <Davit Dovlatian>
2017-01-30 22:03:27 -0800

    sign up page formatted

--- app/src/main/java/com/example/abhinavgianey/picturethis/SignUpActivity.java
+++ app/src/main/java/com/example/abhinavgianey/picturethis/SignUpActivity.java
@@ -2,6 +2,8 @@ package com.example.abhinavgianey.picturethis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +11,28 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        setDropDown();
     }
+    private void setDropDown() {
+        Spinner monthSpinner = (Spinner) findViewById(R.id.month_spinner);
+        ArrayAdapter<CharSequence> monthAdapter = ArrayAdapter.createFromResource(this,
+                R.array.months_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        monthSpinner.setAdapter(monthAdapter);
+        Spinner daySpinner = (Spinner) findViewById(R.id.day_spinner);
+        ArrayAdapter<CharSequence> dayAdapter = ArrayAdapter.createFromResource(this,
+                R.array.days_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        daySpinner.setAdapter(dayAdapter);
+        Spinner yearSpinner = (Spinner) findViewById(R.id.year_spinner);
+        ArrayAdapter<CharSequence> yearAdapter = ArrayAdapter.createFromResource(this,
+                R.array.years_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        yearSpinner.setAdapter(yearAdapter);}
 }
