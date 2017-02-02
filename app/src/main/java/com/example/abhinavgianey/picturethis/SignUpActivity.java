c20d69ecd07fb20cbe63eb6e4077a9f85889af5c
Davit Dovlatian <davitdovlatian@gmail.com>
2017-02-01 22:46:19 -0800

    worked on toggle button

--- app/src/main/java/com/example/abhinavgianey/picturethis/SignUpActivity.java
+++ app/src/main/java/com/example/abhinavgianey/picturethis/SignUpActivity.java
@@ -1,11 +1,16 @@
 package com.example.abhinavgianey.picturethis;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    boolean maleClicked = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +39,33 @@ public class SignUpActivity extends AppCompatActivity {
 // Specify the layout to use when the list of choices appears
         yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
-        yearSpinner.setAdapter(yearAdapter);}
+        yearSpinner.setAdapter(yearAdapter);
+    }
+
+    public void onMaleClick(View view) {
+        if (maleClicked){
+            return;
+        }
+        maleClicked = true;
+        Button maleButton = (Button) view;
+        maleButton.setBackground(ContextCompat.getDrawable(this, R.drawable.bordered_button));
+        maleButton.setTextColor(0xFFFFFFFF);
+        Button femaleButton = (Button) findViewById(R.id.femaleButton);
+        femaleButton.setBackground(ContextCompat.getDrawable(this, R.drawable.bordered_button2));
+        femaleButton.setTextColor(0xFF002C7D);
+
+    }
+
+    public void onFemaleClick(View view) {
+        if (!maleClicked){
+            return;
+        }
+        maleClicked = false;
+        Button femaleButton = (Button) view;
+        femaleButton.setBackground(ContextCompat.getDrawable(this, R.drawable.bordered_button4));
+        femaleButton.setTextColor(0xFFFFFFFF);
+        Button maleButton = (Button) findViewById(R.id.maleButton);
+        maleButton.setBackground(ContextCompat.getDrawable(this, R.drawable.bordered_button3));
+        maleButton.setTextColor(0xFF002C7D);
+    }
 }
